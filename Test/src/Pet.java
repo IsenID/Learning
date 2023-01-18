9328eafaaf5e1f7bf65d6738468029a5feb2cf8d
Денис <isen060@gmail.com>
2023-01-18 22:26:10 +0300

    Добавил рыбу в петомца

--- Test/src/Pet.java
+++ Test/src/Pet.java
@@ -2,6 +2,6 @@ public class Pet {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Какой сложный GIT!!!");
-
+        System.out.println("Тут может быть рыба))");
     }
 }
